751d63b424ff59c3fa44826299e1179216927858
mrvs_deloitte <mrvs@deloitte.com>
2026-06-19 15:55:55 +0530

    Update in return statement

--- src/main/java/org/example/jenkinspoc/Controller/HelloController.java
+++ src/main/java/org/example/jenkinspoc/Controller/HelloController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Jenkins Pipeline-2!";
+        return "Hello from Jenkins Pipeline-v2!";
     }
 }
